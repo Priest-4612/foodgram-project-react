20816a6d4ceed988033551ad4f687a66fbcd04f1
Priest4612 <priest4612@gmail.com>
2022-01-25 00:48:14 +0300

    Добавил представление для пользователя

--- backend/api_foodgram/api/serializers.py
+++ backend/api_foodgram/api/serializers.py
@@ -10,6 +10,49 @@ ERROR_FORBIDDEN_USERNAME = ('Использовать имя "{username}" в к�
                             'username запрещено.')
 
 
+class MeSerializer(serializers.ModelSerializer):
+    is_subscribe = serializers.SerializerMethodField()
+
+    email = serializers.EmailField(
+        max_length=150,
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        max_length=150,
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    first_name = serializers.CharField(
+        max_length=150,
+        required=True
+    )
+    last_name = serializers.CharField(
+        max_length=150,
+        required=True
+    )
+
+    def validate_username(self, value):
+        if value in FORBIDDEN_USERNAME:
+            raise serializers.ValidationError(
+                self.ERROR_FORBIDDEN_USERNAME.format(username=value)
+            )
+        return value
+
+    class Meta:
+        fields = [
+            'username', 'email', 'first_name', 'last_name', 'is_subscribe'
+        ]
+        model = User
+
+    def get_is_subscribe(self, obj):
+        user = self.context['request'].user
+        return (
+            user
+            and obj.followed.filter(user__username=user).exists()
+        )
+
+
 class UserSerualizer(serializers.ModelSerializer):
     is_subscribe = serializers.SerializerMethodField()
 
@@ -40,11 +83,13 @@ class UserSerualizer(serializers.ModelSerializer):
         return value
 
     class Meta:
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = [
+            'username', 'email', 'first_name', 'last_name', 'is_subscribe'
+        ]
         model = User
 
     def get_is_subscribe(self, obj):
-        user = serializers.CurrentUserDefault()
+        user = self.context['request'].user
         return (
             user
             and obj.followed.filter(user__username=user).exists()
