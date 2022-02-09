e9cdfa6ff5c57e0df3cb69c823a6d5c0a653341a
Priest4612 <priest4612@gmail.com>
2022-02-09 22:57:56 +0300

    Обновил представление users

--- backend/api_foodgram/api/views.py
+++ backend/api_foodgram/api/views.py
@@ -1,7 +1,11 @@
 from djoser import views as djoser
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from users.models import User
 
+from .permissions import OwnerOnly
 from .serializers import CustomUserSerializer
 
 
@@ -9,3 +13,9 @@ class UserViewSet(djoser.UserViewSet):
     serializer_class = CustomUserSerializer
     queryset = User.objects.all()
     pagination_class = PageNumberPagination
+
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        user = request.user
+        serializer = CustomUserSerializer(user)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
