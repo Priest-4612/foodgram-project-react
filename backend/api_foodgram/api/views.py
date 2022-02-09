54b786ec5bd83aaebf2cb87b93b4ba4296464db4
Priest4612 <priest4612@gmail.com>
2022-02-09 22:42:08 +0300

    Удалил лишний код

--- backend/api_foodgram/api/views.py
+++ backend/api_foodgram/api/views.py
@@ -1,12 +1,7 @@
 from djoser import views as djoser
-from rest_framework import status
-from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
-from rest_framework.response import Response
 from users.models import User
 
-from .permissions import OwnerOnly, ReadOnly
 from .serializers import CustomUserSerializer
 
 
@@ -14,28 +9,3 @@ class UserViewSet(djoser.UserViewSet):
     serializer_class = CustomUserSerializer
     queryset = User.objects.all()
     pagination_class = PageNumberPagination
-
-    # def get_permissions(self):
-    #     if self.action == 'list':
-    #         permission_classes = [ReadOnly]
-    #     elif self.action == 'retrieve':
-    #         permission_classes = [IsAuthenticated]
-    #     elif self.action == 'create':
-    #         permission_classes = [AllowAny]
-    #     elif self.action == 'me':
-    #         permission_classes = [OwnerOnly]
-    #     else:
-    #         permission_classes = [IsAdminUser]
-    #     return [permission() for permission in permission_classes]
-
-    # @action(detail=False, methods=['get', 'post', 'put', 'patch'])
-    # def me(self, request):
-    #     user = request.user
-    #     if request.method == 'GET':
-    #         serializer = CustomUserSerializer(user)
-    #         return Response(data=serializer.data, status=status.HTTP_200_OK)
-    #     serializer = CustomUserSerializer(user, data=request.data,
-    #                                       partial=True)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(data=serializer.data, status=status.HTTP_200_OK)
