4f6e7fb4680701e2abdf0fb102dfd11a43d2187d
Priest4612 <prie4612@gmail.com>
2022-03-15 17:10:59 +0300

    Удалил лишние комментарии

--- backend/api_foodgram/api_foodgram/urls.py
+++ backend/api_foodgram/api_foodgram/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # path('api/', include('djoser.urls')),
-    # path('api/auth/', include('djoser.urls.authtoken')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
