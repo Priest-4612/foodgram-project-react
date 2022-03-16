213d6f1a68ccab4a72651490af2c7d088a604803
Priest4612 <prie4612@gmail.com>
2022-03-16 21:54:12 +0300

    Обновил фильтры

--- backend/api_foodgram/api/filters.py
+++ backend/api_foodgram/api/filters.py
@@ -16,13 +16,25 @@ class RecipeFilter(filters.FilterSet):
         method='get_is_favorited'
     )
     is_in_shopping_cart = filters.BooleanFilter(
-        widget=widgets.BooleanWidget()
+        method='get_is_in_shopping_cart'
     )
 
     class Meta:
         model = Recipe
         fields = ["author__id", "tags__slug",
                   "is_favorited", "is_in_shopping_cart"]
+
+    def get_is_favorited(self, queryset, name, value):
+        if value:
+            return queryset.filter(is_favorited__user=self.request.user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if value:
+            return queryset.filter(
+                is_in_shopping_cart__user=self.request.user
+            )
+        return queryset
 
 
 class IngredientFilter(SearchFilter):
